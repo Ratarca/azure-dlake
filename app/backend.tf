e1b9324bb719e2f4aa6995d3d1c5b4ff9ab8b478
ratarca <ratarca.rtc@gmail.com>
2024-08-26 18:53:21 -0300

    workflow

--- app/backend.tf
+++ app/backend.tf
@@ -1,10 +1,8 @@
 terraform {
     backend "azurerm" {
-        resource_group_name  = "your_resource_group_name"
-        storage_account_name = "your_storage_account_name"
-        container_name       = "your_container_name"
-        key                  = "your_tfstate_file.tfstate"
-        tenant_id            = "your_tenant_id"
-        subscription_id      = "your_subscription_id"
+        resource_group_name  = "dptfstates"
+        storage_account_name = "dptfdevops"
+        container_name       = "tfstates-dp"
+        key                  = "terraform.tfstate"
     }
 }
