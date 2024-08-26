73b4c976196ef2585f904e03785538eb23e422cc
ratarca <ratarca.rtc@gmail.com>
2024-08-26 12:10:23 -0300

    fix:provider purpose

--- app/provider.tf
+++ app/provider.tf
@@ -1,15 +1,4 @@
+# The providers used inside project use this to setup a many subscriptions to split a billing
 provider "azurerm" {
     features {}
-}
-
-terraform {
-    required_version = ">= 1.5.7"
-    backend "azurerm"{
-        resource_group_name = "value"
-        storage_account_name = "value"
-        container_name = "value"
-        key = "value"
-        tenant_id = "value"
-        subscription_id = "value"
-    }
 }
