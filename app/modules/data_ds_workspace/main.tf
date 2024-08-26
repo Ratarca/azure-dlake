ca5b30453f5df96566cc461b1afef4516bdb2a7a
ratarca <ratarca.rtc@gmail.com>
2024-08-26 16:32:18 -0300

    feat: ds-workspace

--- app/modules/data_ds_workspace/main.tf
+++ app/modules/data_ds_workspace/main.tf
@@ -4,21 +4,21 @@ resource "azurerm_resource_group" "ds_workspace_aml"{
     location = var.location
 }
 
-# Key Vault
-resource "azurerm_key_vault" "aml_kv" {
-  name                = "${var.env}-${var.company_name}-kv-${random_string.postfix.result}"
-  location            = azurerm_resource_group.aml_rg.location
-  resource_group_name = azurerm_resource_group.aml_rg.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
-}
-
 # Application Insights
 resource "azurerm_application_insights" "aml_ai" {
     name                = "${var.env}-${var.company_name}-${random_string.postfix.result}"
     location            = azurerm_resource_group.ds_workspace_aml.location
     resource_group_name = azurerm_resource_group.ds_workspace_aml.name
     application_type    = "web"
+}
+
+# Key Vault
+resource "azurerm_key_vault" "aml_kv" {
+    name                = "${var.env}-${var.company_name}-kv-${random_string.postfix.result}"
+    location            = azurerm_resource_group.ds_workspace_aml.location
+    resource_group_name = azurerm_resource_group.ds_workspace_aml.name
+    tenant_id           = data.azurerm_client_config.current.tenant_id
+    sku_name            = "standard"
 }
 
 # Storage: Account & Containers
@@ -33,5 +33,27 @@ resource "azurerm_container_registry" "aml_acr" {
 }
 
 # ML Workspace
+resource "azurerm_machine_learning_workspace" "aml_ws" {
+    name                    = "${var.env}-${var.company_name}-ws-${random_string.postfix.result}"
+    friendly_name           = 0 #var.workspace_display_name
+    location                = azurerm_resource_group.ds_workspace_aml.location
+    resource_group_name     = azurerm_resource_group.ds_workspace_aml.name
+    application_insights_id = azurerm_application_insights.aml_ai.id
+    key_vault_id            = azurerm_key_vault.aml_kv.id
+    storage_account_id      = 0 #azurerm_storage_account.aml_sa.id
+    container_registry_id   = azurerm_container_registry.aml_acr.id
 
-# Compute Resource
+    identity {
+        type = "SystemAssigned"
+    }
+}
+
+resource "null_resource" "compute_resouces"{
+    provisioner "local-exec" {
+    command="az ml computetarget create amlcompute --max-nodes 1 --min-nodes 0 --name cpu-cluster --vm-size Standard_DS3_v2 --idle-seconds-before-scaledown 600 --assign-identity [system] --resource-group ${azurerm_machine_learning_workspace.aml_ws.resource_group_name} --workspace-name ${azurerm_machine_learning_workspace.aml_ws.name}"
+}
+
+    provisioner "local-exec" {
+    command="az ml computetarget create computeinstance --name ci-${random_string.postfix.result}-test --vm-size Standard_DS3_v2 --resource-group ${azurerm_machine_learning_workspace.aml_ws.resource_group_name} --workspace-name ${azurerm_machine_learning_workspace.aml_ws.name}"
+}
+}
