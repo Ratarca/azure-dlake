6cab0cbb316fe4211fc0b441ac8dd3e5b74d157f
ratarca <ratarca.rtc@gmail.com>
2024-08-28 17:05:53 -0300

    add dependencies

--- app/main.tf
+++ app/main.tf
@@ -8,4 +8,7 @@ module "dlake_ml_workspace"{
     source = "./modules/data_ds_workspace"
     env = "dev"
     company = "wonka"
+
+    # Add this to build on sequence the principal resources
+    depends_on = [ module.dlake_storage ]
 }
