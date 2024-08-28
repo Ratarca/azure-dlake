d2c01db9d46bea4690edd6365b50cb0f09c3deca
ratarca <ratarca.rtc@gmail.com>
2024-08-28 14:06:21 -0300

    feat: Improvement on key-vault delete to follow https://go.microsoft.com/fwlink/?linkid=2147740

--- app/provider.tf
+++ app/provider.tf
@@ -1,5 +1,14 @@
 provider "azurerm" {
-    features {}
+    features {
+        key_vault {
+        recover_soft_deleted_key_vaults    = false
+        purge_soft_delete_on_destroy       = false
+        purge_soft_deleted_keys_on_destroy = false
+        }
+        resource_group {
+        prevent_deletion_if_contains_resources = false
+        }
+    }
 }
 
 # Set the Azure Provider source and version being used
